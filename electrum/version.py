4583dbed799c51f4947c855627ba28b187446cbf
Your Name <you@example.com>
2023-12-08 09:51:31 -0500

    push for new backup nodes

--- electrum/version.py
+++ electrum/version.py
@@ -1,5 +1,5 @@
-ELECTRUM_VERSION = '4.2.3'     # version of the client package
-APK_VERSION = '4.2.3.0'        # read by buildozer.spec
+ELECTRUM_VERSION = '4.2.6'     # version of the client package
+APK_VERSION = '4.2.6.0'        # read by buildozer.spec
 
 PROTOCOL_VERSION = '1.4'     # protocol version requested
 
