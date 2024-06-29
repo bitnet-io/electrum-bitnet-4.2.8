0d30508410f4198335b1389d507c48ed60e51c55
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-06-29 07:17:52 -0700

    Update build-apk-android.sh

--- build-apk-android.sh
+++ build-apk-android.sh
@@ -1,5 +1,10 @@
 sleep 2s
 echo './contrib/android/make_apk'
+echo 'exit back out with exit'
+echo 'then apk debug is in dist/'
+echo 'cp -rf dist/Bitnet*here*apk .'
+echo 'adjust create-keystore to your local apksigner from 29.0.3 or 33.0.1   FROM ANDROID-SDK on local machine command line tools or otherwise or from the docker-android builder in /opt/android/android-sdk'
+echo './create-keystore-and-sign.sh'
 
 docker pull c4pt/electrum-android-build-apks
 
