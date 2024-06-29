48e4fbab9d6184833af5557aa2c1e17d524ca426
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-06-28 21:43:17 -0700

    Update android-builder-docker.sh

--- android-builder-docker.sh
+++ android-builder-docker.sh
@@ -1,4 +1,6 @@
- docker run -it -d --net host --rm \
+docker pull c4pt/electrum-android-build-apks
+
+docker run -it -d --net host --rm \
     -v $PWD:/home/user/wspace/electrum \
     -v $PWD/.buildozer/.gradle:/home/user/.gradle \
     --workdir /home/user/wspace/electrum \
