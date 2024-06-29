19debcfa4da1ed0b594c19a87964c61c6596c70e
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-06-29 07:07:18 -0700

    Update build-binary.sh

--- contrib/build-wine/build-binary.sh
+++ contrib/build-wine/build-binary.sh
@@ -1,6 +1,7 @@
+docker pull c4pt/electrum-wine-builder-img
 docker run -it --net host \
     --name electrum-wine-builder-cont \
     -v $PWD:/opt/wine64/drive_c/electrum \
     --rm \
     --workdir /opt/wine64/drive_c/electrum/contrib/build-wine \
-    electrum-wine-builder-img bash \
+    c4pt/electrum-wine-builder-img bash \
