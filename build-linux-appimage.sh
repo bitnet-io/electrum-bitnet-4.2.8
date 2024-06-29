7bc88aa8b23934acceaea814386c301e96b77d74
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-06-29 07:12:12 -0700

    Update build-linux-appimage.sh

--- build-linux-appimage.sh
+++ build-linux-appimage.sh
@@ -1,5 +1,17 @@
 sleep 5s
-echo './make_appimage.sh'
+echo './contrib/build-linux/appimage/make_appimage.sh'
 
+
+## then docker exec -it <docker-id> bash 
+# this should run from /opt/electrum-bitnet-4.2.8
+#   ./contrib/build-linux/appimage/build-binary.sh
+
+# ./make_appimage.sh
 docker pull c4pt/bullseye-appimage-builder
-./contrib/build-linux/appimage/build-binary.sh
+
+docker run -it --net host \
+    --name electrum-appimage-builder \
+    -v $PWD:/opt/electrum \
+    --rm \
+    --workdir /opt/electrum/contrib/build-linux/appimage \
+    c4pt/bullseye-appimage-builder bash
